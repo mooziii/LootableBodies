def958cc467acaeeea9b5e6680b05d43b81d77aa
mooziii <mooz@moozunity.de>
2021-03-18 09:00:22 +0100

    Initialize MongoManager

--- src/main/kotlin/de/polylymer/LootableBodies.kt
+++ src/main/kotlin/de/polylymer/LootableBodies.kt
@@ -1,6 +1,14 @@
 package de.polylymer
 
+import de.polylymer.database.MongoManager
 import net.axay.kspigot.main.KSpigot
 
 class LootableBodies : KSpigot() {
+
+    lateinit var mongoManager: MongoManager
+
+    override fun startup() {
+        mongoManager = MongoManager
+    }
+
 }
