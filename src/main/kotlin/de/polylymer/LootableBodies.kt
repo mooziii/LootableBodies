9c44de9ff84b4368b5a9933473203dee0b58acea
mooziii <mooz@moozunity.de>
2021-03-18 09:18:34 +0100

    Register listeners

--- src/main/kotlin/de/polylymer/LootableBodies.kt
+++ src/main/kotlin/de/polylymer/LootableBodies.kt
@@ -1,6 +1,8 @@
 package de.polylymer
 
 import de.polylymer.database.MongoManager
+import de.polylymer.listener.DeathManager
+import de.polylymer.listener.LootManager
 import net.axay.kspigot.main.KSpigot
 
 class LootableBodies : KSpigot() {
@@ -9,6 +11,8 @@ class LootableBodies : KSpigot() {
 
     override fun startup() {
         mongoManager = MongoManager
+        DeathManager
+        LootManager
     }
 
 }
