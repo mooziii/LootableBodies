f163da40f139babf2ca220ada22aa0412cdf4a71
mooziii <mooz@moozunity.de>
2021-03-18 08:59:53 +0100

    Set jvmTarget to Java11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     kotlin("jvm") version "1.4.31"
+    kotlin("plugin.serialization") version "1.4.31"
 }
 
 group = "de.polylymer"
@@ -10,11 +11,24 @@ repositories {
     mavenCentral()
     jcenter()
     maven("https://jitpack.io")
+    mavenLocal()
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    //KSPIGOT
     implementation("net.axay", "KSpigot", "v1.16.5_R23")
-    compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
+    //SPIGOT
+    compileOnly("org.spigotmc", "spigot-api", "1.16.4-R0.1-SNAPSHOT")
+    //BLUEUTILS
     compileOnly("net.axay", "BlueUtils", "1.0.2")
+    // KMONGO and MONGODB
+    compileOnly("org.litote.kmongo", "kmongo-core", "4.2.3")
+    compileOnly("org.litote.kmongo", "kmongo-serialization-mapping", "4.2.3")
 }
