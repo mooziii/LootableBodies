ae9d117bf0ab886066f2e2aaa57ab0649cdf3ef9
mooziii <mooz@moozunity.de>
2021-03-27 21:04:55 +0100

    ok

--- src/main/kotlin/de/polylymer/listener/DeathManager.kt
+++ src/main/kotlin/de/polylymer/listener/DeathManager.kt
@@ -2,14 +2,21 @@ package de.polylymer.listener
 
 import de.polylymer.database.LootableBody
 import de.polylymer.database.MongoManager
+import net.axay.kspigot.data.nbtData
 import net.axay.kspigot.event.listen
-import org.bukkit.GameRule
-import org.bukkit.Material
+import net.axay.kspigot.items.meta
+import net.axay.kspigot.runnables.task
+import org.bukkit.*
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.EntityType
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+import org.bukkit.inventory.meta.LeatherArmorMeta
+import org.bukkit.inventory.meta.SkullMeta
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.bukkit.util.EulerAngle
 import java.util.*
 
@@ -33,9 +40,16 @@ object DeathManager {
                 armorStand.rightLegPose = EulerAngle(360.0, 0.0, 0.0)
                 armorStand.leftArmPose = EulerAngle(240.0, 329.0, 41.0)
                 armorStand.rightArmPose = EulerAngle(261.0, 0.0, 265.0)
+                armorStand.isCustomNameVisible = true
+                armorStand.customName = it.entity.name
+                armorStand.addPotionEffect(PotionEffect(PotionEffectType.FIRE_RESISTANCE, Int.MAX_VALUE, 0, false,false))
                 armorStand.addScoreboardTag("isBody:true")
                 armorStand.addScoreboardTag("hasLoot:true")
-                armorStand.equipment!!.helmet = ItemStack(Material.SKELETON_SKULL)
+                val stack = ItemStack(Material.PLAYER_HEAD)
+                var meta = stack.itemMeta as SkullMeta
+                meta.owningPlayer = it.entity
+                stack.itemMeta = meta
+                armorStand.equipment!!.helmet = stack
                 val arrayList = arrayListOf<ItemStack>()
                 for (itemStack in it.drops) {
                     arrayList.add(itemStack)
@@ -48,6 +62,49 @@ object DeathManager {
                     armorStand.uniqueId.toString(),
                     arrayList
                 ))
+                stack.type = Material.LEATHER_CHESTPLATE
+                val leatherMeta = stack.itemMeta as LeatherArmorMeta
+                leatherMeta.setColor(Color.OLIVE)
+                stack.itemMeta = leatherMeta
+                armorStand.equipment!!.chestplate = stack
+                stack.type = Material.LEATHER_LEGGINGS
+                stack.itemMeta = leatherMeta
+                armorStand.equipment!!.leggings = stack
+                it.droppedExp = it.entity.expToLevel
+                var i = 0
+                val event = it
+                task(
+                    period = 3,
+                    howOften = 60
+                ) {
+                    event.entity.world.spawnParticle(Particle.ASH, event.entity.location.clone().add(0.0,1.0,0.0),0)
+                }
+                task(
+                    period = 15*20
+                ) {
+                    if(!armorStand.isDead) {
+                        if(Random().nextBoolean()) {
+                            armorStand.world.playSound(armorStand.location, Sound.ENTITY_ZOMBIE_VILLAGER_CURE, 0.6f, 0f)
+                        } else {
+                            armorStand.world.playSound(armorStand.location, Sound.ENTITY_ZOMBIE_VILLAGER_CURE, 0.6f, 10f)
+                        }
+                        i += 1
+                        if(i == 15) {
+                            armorStand.equipment!!.helmet = ItemStack(Material.SKELETON_SKULL)
+                        }
+                    } else {
+                        it.cancel()
+                    }
+                }
+                task(
+                    delay = 30*60*20
+                ) {
+                    if(event.entity.isDead) {
+                        it.cancel()
+                    } else {
+                        event.entity.remove()
+                    }
+                }
             }
         }
     }
--- src/main/kotlin/de/polylymer/listener/LootManager.kt
+++ src/main/kotlin/de/polylymer/listener/LootManager.kt
@@ -3,10 +3,13 @@ package de.polylymer.listener
 import de.polylymer.database.LootableBody
 import de.polylymer.database.MongoManager
 import net.axay.kspigot.event.listen
+import net.axay.kspigot.runnables.task
+import org.bukkit.Particle
 import org.bukkit.Sound
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.event.player.PlayerInteractAtEntityEvent
 import org.litote.kmongo.bson
 import org.litote.kmongo.findOne
@@ -45,6 +48,35 @@ object LootManager {
                         } else {
                             it.isCancelled = true
                         }
+                        val event = it
+                        task(
+                            period = 3,
+                            howOften = 30
+                        ) {
+                            event.entity.world.spawnParticle(Particle.ASH, event.entity.location,0)
+                        }
+                    }
+                }
+            }
+        }
+        listen<EntityDeathEvent> {
+            if(it.entity is ArmorStand) {
+                if (it.entity.scoreboardTags.contains("isBody:true")) {
+                    if(it.entity.scoreboardTags.contains("hasLoot:true")) {
+                        it.entity.world.playSound(it.entity.location, Sound.ENTITY_LEASH_KNOT_BREAK, 1f,1f)
+                        val lootableBody = MongoManager.BODIES.findOne("{\"uuid\":\"${it.entity.uniqueId.toString()}\"}")!!
+                        for (itemStack in lootableBody.loot) {
+                            it.entity.world.dropItem(it.entity.location, itemStack)
+                        }
+                        it.entity.removeScoreboardTag("hasLoot:true")
+                        val event = it
+                        task(
+                            period = 3,
+                            howOften = 30
+                        ) {
+                            event.entity.world.spawnParticle(Particle.ASH, event.entity.location,0)
+                        }
+                        MongoManager.BODIES.deleteOne("{\"uuid\":\"${it.entity.uniqueId.toString()}\"}".bson)
                     }
                 }
             }
